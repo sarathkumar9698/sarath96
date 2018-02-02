f2a0b2c684aef48d7fdae3fc29976d4727cd58f3
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-02-02 13:59:23 +0530

    Create sarath.c

---
+++ sarath.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+if(a<=1)
+printf("The number is positive%d,a");
+scanf("%d,&a);
+else
+printf("the number is negative%d,a");
+printf("the number is zero%d,a");
+return 0;
+}
