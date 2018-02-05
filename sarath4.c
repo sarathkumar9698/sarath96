22a8d181c1326175992c5dd39a7ded3744fc5e93
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-02-05 16:35:33 +0530

    sarath4

---
+++ sarath4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+{
+if(a<5);
+scanf("%d",&a);
+printf(the numbers are small");
+else(a=1)
+     {
+      printf("The no are equal");
+      }
+      else(a>5)
+      {
+      printf("the numbers are large");
+      }
+      }
+      getch();
+      return 0;
+      }
+
