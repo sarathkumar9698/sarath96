37a788747469bfb361c86d3dbda922343da658a1
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-02-05 16:20:51 +0530

    sarath3

---
+++ sarath3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+if(a<1);
+printf("The charater are alphabets");
+scanf("%d",&a);
+else
+printf("the character are not alphates");
+getch()
+return 0;
+}
