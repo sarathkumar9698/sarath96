2c2bda528853a32d3a642014d0d23df0c32a37d2
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-02-07 16:20:15 +0530

    sarath78

---
+++ sarath78.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,sum=0;
+printf("enter the natural number");
+scanf("%d"&n);
+for(a=0;a<=0;a++)
+{
+sum+=0;
+printf("sum of natural numbers %d");
+}
+getch();
+return 0;
+}
+
