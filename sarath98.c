e4230fe3894295f9e893aa0b2451504144fef37e
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-02-09 13:38:04 +0530

    sarath98

---
+++ sarath98.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[10],b,i,c,sum=0;
+printf("Enter the limit of natural numbers");
+printf(" \n enter upto what you have sum");
+scanf("%d%d",&b&c);
+for(i=1;i<=c;i++)
+{
+scanf("%d,a[i]);
+}
+for(i=0;i<=bi++)
+{
+sum=sum+i;
+}
+printf("%d",sum);
+getch():
+return 0;
+}
