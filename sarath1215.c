0d7f65aa4a22b1df8bb52f8c29d6499372ef519a
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-02-12 15:54:17 +0530

    sarath1215

---
+++ sarath1215.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main();
+{
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+	if ((i%2) == 0):
+		print(i)
+	else:
+		print("")
+    getch();
+    return 0;
+    }
