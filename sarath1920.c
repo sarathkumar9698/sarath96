271028d2faf1a3e7adb6ec712a329c40e4175368
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-02-19 16:02:30 +0530

    sarath1920

---
+++ sarath1920.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     int n,i;
+     printf("enter the number",n)
+     for(i=1;i<n;i++)
+  {
+     if(i%5==0)
+{
+     printf("%d\n",i);
+  }
+} 
+getch();
+return 0;
+}
