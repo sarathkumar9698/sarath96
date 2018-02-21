019f32bcb389cabab83c96fa0b2e510f2beffb02
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-02-21 16:16:39 +0530

    sarath2129.c

---
+++ sarath2129.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,sum=0;
+	printf("ENTER THE  NUMBER  :\n");
+	scanf("%d",&a);
+	sum=(a*(a-1))/2;
+	printf("THE SUM OF NOS %d IS %d",a,sum);
+  getch();
+}
