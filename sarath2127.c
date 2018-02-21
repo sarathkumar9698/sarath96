784d61e053eb2832aec531c4d5d6415e8567b855
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-02-21 16:06:31 +0530

    sarath2127

---
+++ sarath2127.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+	char n;
+	printf("enter the string:",n);
+	scanf("%s",&n);
+	if(n>=65&&n<=90||n>=97&&n<=122)
+	{
+		printf("no");
+	}
+	else
+	{
+		printf("yes");
+	}
+  getch();
+	return 0;
+}
