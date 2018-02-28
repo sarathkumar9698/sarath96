ba1f029cbcd9309507f0ab133564053ee8417e07
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-02-28 15:43:52 +0530

    sarath2833

---
+++ sarath2833.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	char s[100];
+	int i,j,count=0;
+	scanf("%[^\n]s",s);
+	for(j=0;s[j]!='\0';++j)
+	{
+		
+	}
+	for(i=0;i<j;i++)
+	{
+	
+	if(s[i]==' ')
+	{
+		count++;
+	}
+		
+	}
+	printf("%d",count);
+  getch();
+	return 0;
+}
