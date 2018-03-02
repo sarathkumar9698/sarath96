9329784ad03362c68b7bc4806e682bcf03167fbe
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-03-02 13:19:30 +0530

    sarath339

---
+++ sarath339.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+  int main() {
+    int a[10];
+    int i;
+    int greatest;
+    printf("Enter ten values:");
+
+    for (i = 0; i < 10; i++) {
+    scanf("%d", &a[i]);
+    }
+    
+    greatest = a[0];
+    for (i = 0; i < 10; i++) {
+if (a[i] > greatest) {
+greatest = a[i];
+    }
+    }
+    printf(" %d", greatest);
+    getch();
+    return 0;
+  }
