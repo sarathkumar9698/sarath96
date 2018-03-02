f16a2caae9453cf09d63e46cec35efedae437177
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-03-02 13:24:16 +0530

    sarath341

---
+++ sarath341.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	char s[50];
+	scanf("%s",s);
+	int n,i;
+	scanf("\n%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("\n%s",s);
+	}
+  getch();
+	return 0;
+}
