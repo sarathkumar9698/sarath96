3581b3894973a97b15a1ed9c05a5b600402c8014
sarathkumar9698 <35688584+sarathkumar9698@users.noreply.github.com>
2018-03-12 15:41:59 +0530

    Create sarath1248.c

---
+++ sarath1248.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+	int n,sum=0,i,average;
+	scanf("%d",&n);
+	int a[1000];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);	
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+a[i];
+	}
+	average=sum/n;
+	printf("%d",average);
+	getch();
+	return 0;
+}
